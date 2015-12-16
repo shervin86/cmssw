091ee7a7e9a9300284917d47bd509d1ce5914240
Shervin <shervin@cern.ch>
2015-12-16 11:46:59 +0100

    python to create EcalIntercalibConstants sqlite file adding some instructions about how to create sqlite file for EcalIntercalibConstants

--- updateEcalFloatCondtion_test.py
+++ updateEcalFloatCondtion_test.py
@@ -1,16 +1,3 @@
-# 
-#
-# Read from xml and insert into database using
-# PopCon 
-#
-# This is a template, generate real test using
-#
-# sed 's/EcalIntercalibConstants/your-record/g' testTemplate.py > testyourrecord.py
-#
-# Stefano Argiro', $Id: testEcalIntercalibConstants.py,v 1.1 2008/11/14 15:46:03 argiro Exp $
-# Modified by Shervin Nourbakhsh 
-#
-
 import FWCore.ParameterSet.Config as cms
 import os, sys, imp, re
 import FWCore.ParameterSet.VarParsing as VarParsing
@@ -25,16 +12,20 @@ options.register('tag',
                  VarParsing.VarParsing.varType.string,
                  "name of the tag")
 options.register('firstRun',
-                 1,
+                 '1',
                  VarParsing.VarParsing.multiplicity.singleton,
-                 VarParsing.VarParsing.varType.int,
+                 VarParsing.VarParsing.varType.string,
                  "")
 options.register('fileName',
                  '',
                  VarParsing.VarParsing.multiplicity.singleton,
                  VarParsing.VarParsing.varType.string,
                  "xml file path")
-                 
+options.register('record',
+                 'EcalIntercalibConstantsRcd',
+                 VarParsing.VarParsing.multiplicity.singleton,
+                 VarParsing.VarParsing.varType.string,
+                 "record: EcalIntercalibConstantsRcd or EcalTimeCalibConstantsRcd")
 ### setup any defaults you want
 options.output="output/ecalTiming.root"
 options.secondaryOutput="ntuple.root"
@@ -42,10 +33,7 @@ options.secondaryOutput="ntuple.root"
 options.parseArguments()
 print options
 
-process = cms.Process("TEST")
-
-
-tagName = options.tag
+process = cms.Process("ProcessOne")
 
 process.MessageLogger = cms.Service("MessageLogger",
     debugModules = cms.untracked.vstring('*'),
@@ -55,45 +43,49 @@ process.MessageLogger = cms.Service("MessageLogger",
     destinations = cms.untracked.vstring('cout')
 )
 
-
-process.load("CondCore.DBCommon.CondDBCommon_cfi")
-process.CondDBCommon.connect = 'sqlite_file:'+tagName+'.db'
-process.CondDBCommon.DBParameters.authenticationPath = cms.untracked.string('/afs/cern.ch/cms/DB/conddb')
-
 process.source = cms.Source("EmptyIOVSource",
+    lastValue = cms.uint64(100000000000),
     timetype = cms.string('runnumber'),
     firstValue = cms.uint64(100000000000),
-    lastValue = cms.uint64(100000000000),
     interval = cms.uint64(1)
 )
 
+process.load("CondCore.DBCommon.CondDBCommon_cfi")
+
+tagName = options.tag
+
+process.CondDBCommon.connect = 'sqlite_file:'+tagName+'.db'
 
 process.PoolDBOutputService = cms.Service("PoolDBOutputService",
-    process.CondDBCommon,
-    #timetype = cms.untracked.string('runnumber'),
-    toPut = cms.VPSet(
-        cms.PSet(
-            record = cms.string('EcalIntercalibConstantsRcd'),
-            tag = cms.string(tagName)
-            )
-        ),
-  logconnect= cms.untracked.string('sqlite_file:logtestEcalIntercalibConstants.db')                                     
+  process.CondDBCommon, 
+  logconnect = cms.untracked.string('sqlite_file:log.db'),   
+  toPut = cms.VPSet(
+    cms.PSet(
+      record = cms.string(options.record),
+      tag = cms.string(tagName)
+    )
+  )
 )
 
-process.mytest = cms.EDAnalyzer("EcalIntercalibConstantsAnalyzer",
-    record = cms.string('EcalIntercalibConstantsRcd'),
+# process.Test1 = cms.EDAnalyzer("ExTestEcalIntercalibAnalyzer",
+#   record = cms.string(options.record),
+#   loggingOn= cms.untracked.bool(True),
+#   IsDestDbCheckedInQueryLog=cms.untracked.bool(True),
+#   SinceAppendMode=cms.bool(True),
+#   Source=cms.PSet(
+#     InputFile = cms.string(options.fileName),
+#     firstRun = cms.string(options.firstRun),
+#   )                            
+# )
+
+process.mytest = cms.EDAnalyzer("EcalTimeCalibConstantsAnalyzer",
+    record = cms.string('EcalTimeCalibConstantsRcd'),
     loggingOn= cms.untracked.bool(True),
     SinceAppendMode=cms.bool(True),
     Source=cms.PSet(
-        InputFile = cms.string(options.fileName),
-        #firstRun = cms.string(options.firstRun),
-        xmlFile = cms.untracked.string(options.fileName),
-        since = cms.untracked.int64(options.firstRun),
-        )                            
+    xmlFile = cms.untracked.string(options.fileName),
+    since = cms.untracked.int64(100000),
+    )                            
 )
 
 process.p = cms.Path(process.mytest)
-
-
-
-
